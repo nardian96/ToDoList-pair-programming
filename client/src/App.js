9b25653327927c25f9109aefe2ed78d531aeb9f4
felixf3600 <felixf3600@gmail.com>
2020-08-20 16:17:25 -0700

    state and placeholder functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./Sass/App.css";
+import Todo from "./components/Todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todo: [],
+  };
+
+  eventListeners = [
+    (postTodo = (event) => {
+      event.preventDefault();
+    }),
+    (deleteTodo = (event) => {
+      event.preventDefault();
+    }),
+    (modifyTodo = (event) => {
+      event.preventDefault();
+    }),
+  ];
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Todo list={this.state.todo} eventListeners={this.eventListeners} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
