9e3c029c9def0cf62e997df28f57d20e0561cbb4
Brian Haubrick <bhaubrick@telus.net>
2020-08-20 16:00:03 -0700

    I have finished the todosController please have a look to see if we are on the same page

--- server/controllers/todosController.js
+++ server/controllers/todosController.js
@@ -1,7 +1,36 @@
-import React from 'react';
+const todos = require('../models/todosModel); 
 
-function todosController() {
-  return <div></div>;
+function listTodos(_req, res) {
+    res.json(todos.list());
 }
 
-export default todosController;
+function getTodosByID(req, res) {
+    res.json(todos.getByID)(req.params.id));
+}
+
+function addTodo(req, res) {
+    if (!req.body.id || !req.body.title || !req.body.description || !req.body.date) {
+        res.status(400).send({
+            error: 'Post body must contain all requiredProperties',
+            requiredProperties: ['id', 'title', 'descrition', 'date'],
+        })
+    }
+    res.json(todos.add(req.body));
+}
+
+function deleteTodo(req, res) {
+    res.json(todos.deleteTodo(req.perems.id);
+}
+
+function updateTodo(req, res) {
+    res.json(todos.updateTodo(res.params.id, req.body));
+}
+
+module.exports = {
+    listProducts,
+    addProduct,
+    getProductByID,
+    deleteProduct,
+    updateProduct,
+  };
+  
